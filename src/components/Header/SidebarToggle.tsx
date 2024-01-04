57443eae5f7d5bfcdec653a93e44a9e1c1f2a665
Alexandre Gaubert <alexandre.gaubert@mergify.com>
2024-01-04 10:15:54 +0100

    fix: Navbar toggle on mobile

--- src/components/Header/SidebarToggle.tsx
+++ src/components/Header/SidebarToggle.tsx
@@ -1,9 +1,28 @@
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import './SidebarToggle.css';
 import './HeaderButton.css';
 
 const MenuToggle = () => {
 	const [sidebarShown, setSidebarShown] = useState(false);
+
+	useEffect(() => {
+		const body = document.getElementsByTagName('body')[0];
+		if (sidebarShown) {
+			body.classList.add('mobile-sidebar-toggle');
+			document.querySelectorAll('aside nav details').forEach((e) => {
+				e.removeAttribute('open');
+			});
+			document
+				.querySelector('details a[data-current-parent="true"]')
+				?.closest('details')
+				?.setAttribute('open', '');
+		} else {
+			body.classList.remove('mobile-sidebar-toggle');
+			document.querySelectorAll('aside nav details').forEach((e) => {
+				e.setAttribute('open', '');
+			});
+		}
+	}, [sidebarShown]);
 
 	return (
 		<button
