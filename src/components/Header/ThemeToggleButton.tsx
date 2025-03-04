52ad6cd699e9c7fd5bdfb28fdec05df1d87ff7ea
Mehdi ABAAKOUK <sileht@sileht.net>
2025-03-04 21:11:52 +0100

    chore(deps): drop preact
    No need to have two frameworks.
    
    Change-Id: I2841bff3a6b271fc3796eb63b8430da1e036bdc6

--- src/components/Header/ThemeToggleButton.tsx
+++ src/components/Header/ThemeToggleButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useState } from 'react';
 import './ThemeToggleButton.css';
 import { FiMoon, FiSun } from 'react-icons/fi';
 
